e9298f2aa36ceb1900e2d49a8361bb1109b5c0f5
Marcos Antonio <marcosantoniotaina@gmail.com>
2019-12-16 01:08:03 -0300

    add devise and changes in layout

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_15_060237) do
+ActiveRecord::Schema.define(version: 2019_12_16_025221) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -75,6 +75,18 @@ ActiveRecord::Schema.define(version: 2019_12_15_060237) do
     t.integer "item_19"
     t.string "report"
     t.text "tooth"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   create_table "works", force: :cascade do |t|
