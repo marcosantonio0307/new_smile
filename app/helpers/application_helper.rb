f3b67484fbf3462a798a6a5e8376a2f65e238c40
Marcos Antonio <marcosantoniotaina@gmail.com>
2020-01-15 14:44:17 -0300

    test 6 image

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,14 @@ module ApplicationHelper
 		  money = number_to_currency money
 		end
 	end
+
+	def wicked_image_active_storage_workaround( image )
+	  if image.is_a? ActiveStorage::Attachment
+	    save_path = Rails.root.join( "tmp", "#{image.id}.jpg")
+	    File.open(save_path, 'wb') do |file|
+	      file << image.blob.download
+	    end
+	    return save_path.to_s
+	  end
+	end
 end
